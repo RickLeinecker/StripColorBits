df4d729acb3303706e5ed0e5506f36cd96817d09
RickLeinecker <Rick@JSVentures.com>
2017-09-29 10:44:01 -0400

    Add files via upload

---
+++ StripColorBits/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace StripColorBits
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmbBits = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnSelectColor = new System.Windows.Forms.Button();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.SuspendLayout();
+			// 
+			// cmbBits
+			// 
+			this.cmbBits.FormattingEnabled = true;
+			this.cmbBits.Items.AddRange(new object[] {
+            "None",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+			this.cmbBits.Location = new System.Drawing.Point(646, 151);
+			this.cmbBits.Name = "cmbBits";
+			this.cmbBits.Size = new System.Drawing.Size(121, 21);
+			this.cmbBits.TabIndex = 0;
+			this.cmbBits.SelectedIndexChanged += new System.EventHandler(this.OnSelectionChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(580, 154);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Bits to Strip";
+			// 
+			// btnSelectColor
+			// 
+			this.btnSelectColor.Location = new System.Drawing.Point(484, 149);
+			this.btnSelectColor.Name = "btnSelectColor";
+			this.btnSelectColor.Size = new System.Drawing.Size(75, 23);
+			this.btnSelectColor.TabIndex = 2;
+			this.btnSelectColor.Text = "Select Color";
+			this.btnSelectColor.UseVisualStyleBackColor = true;
+			this.btnSelectColor.Click += new System.EventHandler(this.btnSelectColor_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(924, 237);
+			this.Controls.Add(this.btnSelectColor);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cmbBits);
+			this.Name = "Form1";
+			this.Text = "Strip Color Bits";
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox cmbBits;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnSelectColor;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+	}
+}
+
---
+++ StripColorBits/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StripColorBits
+{
+	public partial class Form1 : Form
+	{
+
+		Color mainColor = Color.Blue, strippedColor = Color.Blue;
+		int bits = 0xff;
+
+		public Form1()
+		{
+			InitializeComponent();
+			cmbBits.SelectedIndex = 0;
+		}
+
+		int mainRedChannel, mainGreenChannel, mainBlueChannel, strippedRedChannel, strippedGreenChannel, strippedBlueChannel;
+		string mainRedBits, mainGreenBits, mainBlueBits, strippedRedBits, strippedGreenBits, strippedBlueBits;
+		void CreateDisplayDataFromColors()
+		{
+			mainRedChannel = (int)mainColor.R;
+			mainGreenChannel = (int)mainColor.G;
+			mainBlueChannel = (int)mainColor.B;
+			strippedRedChannel = mainRedChannel & bits;
+			strippedGreenChannel = mainGreenChannel & bits;
+			strippedBlueChannel = mainBlueChannel & bits;
+
+			mainRedBits = ConvertToBits(mainRedChannel);
+			mainGreenBits = ConvertToBits(mainGreenChannel);
+			mainBlueBits = ConvertToBits(mainBlueChannel);
+			strippedRedBits = ConvertToBits(strippedRedChannel);
+			strippedGreenBits = ConvertToBits(strippedGreenChannel);
+			strippedBlueBits = ConvertToBits(strippedBlueChannel);
+		}
+
+		string ConvertToBits( int value )
+		{
+			string ret = String.Empty;
+			int bit = 0x80;
+
+			for( int i=0; i<8; i++ )
+			{
+				if((value&bit)!= 0 )
+				{
+					ret += "1 ";
+				}
+				else
+				{
+					ret += "0 ";
+				}
+				bit >>= 1;
+			}
+
+			return (ret);
+		}
+
+		void PaintTheData(Graphics g)
+		{
+			CreateDisplayDataFromColors();
+
+			Color main = Color.FromArgb(mainRedChannel, mainGreenChannel, mainBlueChannel);
+			Color stripped = Color.FromArgb(strippedRedChannel, strippedGreenChannel, strippedBlueChannel);
+
+			SolidBrush mainBrush = new SolidBrush(main);
+			SolidBrush strippedBrush = new SolidBrush(stripped);
+			SolidBrush blackBrush = new SolidBrush(Color.Black);
+			Pen blackPen = new Pen(Color.Black, 5);
+			Font fnt = new Font(FontFamily.GenericSansSerif, 15);
+
+			g.FillRectangle(mainBrush, 20, 20, 100, 100);
+			g.DrawRectangle(blackPen, 20, 20, 100, 100);
+			g.FillRectangle(strippedBrush, 150+20, 20, 100, 100);
+			g.DrawRectangle(blackPen, 150 + 20, 20, 100, 100);
+
+			g.DrawString("Original Red:", fnt, blackBrush, 320, 20);
+			g.DrawString("Original Green:", fnt, blackBrush, 300, 50);
+			g.DrawString("Original Blue:", fnt, blackBrush, 313, 80);
+			g.DrawString(mainRedBits, fnt, blackBrush, 313+130, 20);
+			g.DrawString(mainGreenBits, fnt, blackBrush, 313 + 130, 50);
+			g.DrawString(mainBlueBits, fnt, blackBrush, 313 + 130, 80);
+
+			g.DrawString("Stripped Red:", fnt, blackBrush, 320+300, 20);
+			g.DrawString("Stripped Green:", fnt, blackBrush, 300 + 300, 50);
+			g.DrawString("Stripped Blue:", fnt, blackBrush, 313 + 300, 80);
+			g.DrawString(strippedRedBits, fnt, blackBrush, 313 + 130 + 300, 20);
+			g.DrawString(strippedGreenBits, fnt, blackBrush, 313 + 130 + 300, 50);
+			g.DrawString(strippedBlueBits, fnt, blackBrush, 313 + 130 + 300, 80);
+		}
+
+		private void OnPaint(object sender, PaintEventArgs e)
+		{
+			PaintTheData(e.Graphics);
+		}
+
+		void CreateStrippedColor()
+		{
+			bits = 0xff;
+			for (int i = 0; i < cmbBits.SelectedIndex; i++)
+			{
+				bits <<= 1;
+				bits &= 0xff;
+			}
+			strippedRedChannel = mainRedChannel & bits;
+			strippedGreenChannel = mainGreenChannel & bits;
+			strippedBlueChannel = mainBlueChannel & bits;
+			strippedColor = Color.FromArgb(strippedRedChannel, strippedGreenChannel, strippedBlueChannel);
+		}
+
+		private void OnSelectionChanged(object sender, EventArgs e)
+		{
+			CreateStrippedColor();
+			Invalidate();
+			Update();
+		}
+
+		private void btnSelectColor_Click(object sender, EventArgs e)
+		{
+			DialogResult result = colorDialog1.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				mainColor = colorDialog1.Color;
+				CreateStrippedColor();
+				Invalidate();
+				Update();
+			}
+		}
+	}
+}
